546fd94d32f5c702193e36089b9e51f841a1f817
joaorzz <dev.joaosilva@gmail.com>
2023-10-01 16:13:27 -0300

    feat: create writable repository

---
+++ Infrastructure/Repositories/Writable/UserWritableRepository.cs
@@ -0,0 +1,39 @@
+﻿using Dapper;
+using Domain.Entities;
+using Domain.Repositories.Writable;
+using Infrastructure.Data;
+using System.Data;
+
+namespace Infrastructure.Repositories.Writable
+{
+    public class UserWritableRepository : IUserWritableRepository
+    {
+        private readonly IDbConnectionFactory _dbConnectionFactory;
+
+        public UserWritableRepository(IDbConnectionFactory dbConnectionFactory)
+        {
+            _dbConnectionFactory = dbConnectionFactory;
+        }
+
+        public async Task<int> Create(User user)
+        {
+            using (IDbConnection connection = _dbConnectionFactory.CreateConnection())
+            {
+                const string query = "INSERT INTO \"User\" (\"Name\", \"Address\", \"CEP\", \"Email\", \"Cpf\", \"Phone\", \"City\", \"State\") " +
+                             "VALUES (@Name, @Address, @CEP, @Email, @Cpf, @Phone, @City, @State)";
+
+                return await connection.ExecuteAsync(query, user);
+            }
+        }
+
+        public Task<int> Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> Update(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
