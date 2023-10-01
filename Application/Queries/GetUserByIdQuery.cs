213222c95a742e464291393fa6db8d1e56c6c6fb
joaorzz <dev.joaosilva@gmail.com>
2023-10-01 16:10:02 -0300

    feat: create queries

---
+++ Application/Queries/GetUserByIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using Application.DTOs;
+using MediatR;
+
+namespace Application.Queries
+{
+    public class GetUserByIdQuery : IRequest<UserDTO>
+    {
+        public Guid Id { get; set; }
+
+        public GetUserByIdQuery(Guid id)
+        {
+            Id = id;
+        }
+    }
+}
