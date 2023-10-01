0429f45803d99697a8ee1d6d9971f82b83111aa3
joaorzz <dev.joaosilva@gmail.com>
2023-10-01 16:11:47 -0300

    feat: create usercommand handler

---
+++ Application/Handlers/Commands/CreateUserCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using Application.Commands;
+using Domain.Entities;
+using Domain.Services.Interfaces;
+using MediatR;
+
+namespace Application.Handlers.Commands
+{
+    public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, int>
+    {
+        private readonly IUserWritableService _userWritableService;
+
+        public CreateUserCommandHandler(IUserWritableService userWritableService)
+        {
+            _userWritableService = userWritableService;
+        }
+
+        public async Task<int> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+        {
+            User user = new User(request.Name, request.Address, request.CEP, request.Email, request.Cpf, request.Phone, request.City, request.State);
+
+            return await _userWritableService.CreateUser(user);
+        }
+    }
+}
