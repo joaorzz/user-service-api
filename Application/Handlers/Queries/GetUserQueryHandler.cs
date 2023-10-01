b295a28d3cfd4507b58690da650599305fc6d142
joaorzz <dev.joaosilva@gmail.com>
2023-10-01 16:10:24 -0300

    feat: create query handlers

---
+++ Application/Handlers/Queries/GetUserQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using Application.DTOs;
+using Application.Queries;
+using Domain.Entities;
+using Domain.Services.Interfaces;
+using MediatR;
+
+namespace Application.Handlers.Queries
+{
+    public class GetUserQueryHandler : IRequestHandler<GetUserQuery, UsersResultDTO>
+    {
+        private readonly IUserReadOnlyService _userManagementService;
+
+        public GetUserQueryHandler(IUserReadOnlyService userManagementService)
+        {
+            _userManagementService = userManagementService;
+        }
+
+        public async Task<UsersResultDTO> Handle(GetUserQuery request, CancellationToken cancellationToken)
+        {
+            List<User> users = await _userManagementService.GetAllUsers();
+
+            return new UsersResultDTO(users);
+        }
+    }
+}
