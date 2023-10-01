6e6e6b288dc663b36248a28177400e0d4ac92f7b
joaorzz <dev.joaosilva@gmail.com>
2023-10-01 16:09:32 -0300

    feat: create dtos

---
+++ Application/DTOs/UserDTO.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Entities;
+
+namespace Application.DTOs
+{
+    public class UserDTO
+    {
+        public Guid Id { get; }
+        public string Name { get; }
+        public string Email { get; }
+        public string City { get; }
+        public string State { get; }
+        public DateTime CreatedAt { get; }
+
+        public UserDTO(User user)
+        {
+            Id = user.Id;
+            Name = user.Name;
+            Email = user.Email;
+            City = user.City;
+            State = user.State;
+            CreatedAt = user.CreatedAt;
+        }
+    }
+}
