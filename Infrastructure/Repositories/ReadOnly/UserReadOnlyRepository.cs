82a8a05313ca195af5707fd848a1029615259c78
joaorzz <dev.joaosilva@gmail.com>
2023-10-01 16:13:10 -0300

    feat: create readonly repository

---
+++ Infrastructure/Repositories/ReadOnly/UserReadOnlyRepository.cs
@@ -0,0 +1,48 @@
+﻿using Dapper;
+using Domain.Entities;
+using Domain.Repositories.ReadOnly;
+using Infrastructure.Data;
+using System.Data;
+
+namespace Infrastructure.Repositories.ReadOnly
+{
+    public class UserReadOnlyRepository : IUserReadOnlyRepository
+    {
+        private readonly IDbConnectionFactory _dbConnectionFactory;
+
+        public UserReadOnlyRepository(IDbConnectionFactory dbConnectionFactory)
+        {
+            _dbConnectionFactory = dbConnectionFactory;
+        }
+
+        public async Task<IEnumerable<User>> GetAll()
+        {
+            using (IDbConnection connection = _dbConnectionFactory.CreateConnection())
+            {
+                const string query = "SELECT * FROM \"User\"";
+
+                return await connection.QueryAsync<User>(query);
+            }
+        }
+
+        public async Task<User> GetById(Guid id)
+        {
+            using (IDbConnection connection = _dbConnectionFactory.CreateConnection())
+            {
+                const string query = "SELECT * FROM \"User\" WHERE \"Id\" = @UserId";
+
+                return await connection.QueryFirstAsync<User>(query, new { UserId = id });
+            }
+        }
+
+        public async Task<User> GetByCpf(string cpf)
+        {
+            using (IDbConnection connection = _dbConnectionFactory.CreateConnection())
+            {
+                const string query = "SELECT * FROM \"User\" WHERE \"Cpf\" = @Cpf";
+
+                return await connection.QueryFirstAsync<User>(query, new { Cpf = cpf });
+            }
+        }
+    }
+}
