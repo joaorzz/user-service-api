972078525a3d7b308db34ee4faa81b994008312f
joaorzz <dev.joaosilva@gmail.com>
2023-10-01 16:12:48 -0300

    feat: add repositories interfaces

---
+++ Domain/Repositories/Writable/IUserWritableRepository.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Entities;
+
+namespace Domain.Repositories.Writable
+{
+    public interface IUserWritableRepository
+    {
+        Task<int> Create(User user);
+        Task<int> Update(User user);
+        Task<int> Delete(Guid id);
+    }
+}
