76c8c04a0ee10cf2a9e46b1c34e1bce0b458bdf5
joaorzz <dev.joaosilva@gmail.com>
2023-10-09 07:35:33 -0300

    refactor: add db service in readonly repository

--- Infrastructure/Repositories/ReadOnly/UserReadOnlyRepository.cs
+++ Infrastructure/Repositories/ReadOnly/UserReadOnlyRepository.cs
@@ -1,48 +1,34 @@
-﻿using Dapper;
-using Domain.Entities;
+﻿using Domain.Entities;
 using Domain.Repositories.ReadOnly;
 using Infrastructure.Data;
-using System.Data;
 
 namespace Infrastructure.Repositories.ReadOnly
 {
     public class UserReadOnlyRepository : IUserReadOnlyRepository
     {
-        private readonly IDbConnectionFactory _dbConnectionFactory;
+        private readonly IDbService _dbService;
 
-        public UserReadOnlyRepository(IDbConnectionFactory dbConnectionFactory)
+        public UserReadOnlyRepository(IDbService dbService)
         {
-            _dbConnectionFactory = dbConnectionFactory;
+            _dbService = dbService;
         }
 
         public async Task<IEnumerable<User>> GetAll()
         {
-            using (IDbConnection connection = _dbConnectionFactory.CreateConnection())
-            {
-                const string query = "SELECT * FROM \"User\"";
-
-                return await connection.QueryAsync<User>(query);
-            }
+            const string query = "SELECT * FROM \"User\"";
+            return await _dbService.QueryAsync<User>(query);
         }
 
         public async Task<User> GetById(Guid id)
         {
-            using (IDbConnection connection = _dbConnectionFactory.CreateConnection())
-            {
-                const string query = "SELECT * FROM \"User\" WHERE \"Id\" = @UserId";
-
-                return await connection.QueryFirstAsync<User>(query, new { UserId = id });
-            }
+            const string query = "SELECT * FROM \"User\" WHERE \"Id\" = @UserId";
+            return await _dbService.QueryFirstAsync<User>(query, new { UserId = id });
         }
 
         public async Task<User> GetByCpf(string cpf)
         {
-            using (IDbConnection connection = _dbConnectionFactory.CreateConnection())
-            {
-                const string query = "SELECT * FROM \"User\" WHERE \"Cpf\" = @Cpf";
-
-                return await connection.QueryFirstAsync<User>(query, new { Cpf = cpf });
-            }
+            const string query = "SELECT * FROM \"User\" WHERE \"Cpf\" = @Cpf";
+            return await _dbService.QueryFirstAsync<User>(query, new { Cpf = cpf });
         }
     }
 }
