101b3b901971c4f7cd5ae38c105e69fa5c37cdf3
joaorzz <dev.joaosilva@gmail.com>
2023-10-13 14:00:47 -0300

    feat: add log in delete operation

--- Application/Handlers/Commands/DeleteUserCommandHandler.cs
+++ Application/Handlers/Commands/DeleteUserCommandHandler.cs
@@ -1,21 +1,26 @@
 ﻿using Application.Commands;
 using Domain.Services.Interfaces;
 using MediatR;
+using Microsoft.Extensions.Logging;
 
 namespace Application.Handlers.Commands
 {
     public class DeleteUserCommandHandler : IRequestHandler<DeleteUserCommand>
     {
         private readonly IUserWritableService _userWritableService;
+        private ILogger<DeleteUserCommandHandler> _logger;
 
-        public DeleteUserCommandHandler(IUserWritableService userWritableService)
+        public DeleteUserCommandHandler(IUserWritableService userWritableService, ILogger<DeleteUserCommandHandler> logger)
         {
             _userWritableService = userWritableService;
+            _logger = logger;
         }
 
         public async Task Handle(DeleteUserCommand request, CancellationToken cancellationToken)
         {
             await _userWritableService.DeleteUser(request.Id);
+
+            _logger.LogInformation($"User with id {request.Id} was deleted.");
         }
     }
 }
